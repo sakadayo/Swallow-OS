ae89cb25bf86cc48449da11c270da15330df265b
sakadayo <sakadayo1210@outlook.jp>
2025-09-02 22:08:16 +0900

    Fix showing from only 'A' to 'Hello, World!'

--- BootInBios.asm
+++ BootInBios.asm
@@ -2,11 +2,21 @@
 [ORG 0x7C00]
 
 start:
+    mov si, message
+
+.print_char:
+    lodsb
+    cmp al, 0
+    je .done
     mov ah, 0x0E
-    mov al, 'A'
     int 0x10
+    jmp .print_char
+
+.done:
     cli
     hlt
+
+message db 'Hello, World', 0
 
 times 510 - ($ - $$) db 0
 dw 0xAA55
