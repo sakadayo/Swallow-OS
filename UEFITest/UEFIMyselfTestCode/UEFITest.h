a468b4f21c1ed0b8e3ed0f7548ed4d9fc726513e
sakadayo <sakadayo1210@outlook.jp>
2025-12-25 12:09:10 +0900

    UEFIのテストコードを記述し、ビルド(後から動作を確認)

--- UEFITest/UEFIMyselfTestCode/UEFITest.h
+++ UEFITest/UEFIMyselfTestCode/UEFITest.h
@@ -4,24 +4,26 @@
 #define IN
 #define OUT
 #define EFIAPI
+typedef void VOID;
+typedef int INT32;
+typedef unsigned char UINT8;
+typedef unsigned short UINT16;
+typedef unsigned int UINT32;
+typedef unsigned long long UINT64;
+typedef UINT64 UINTN;
+typedef unsigned short CHAR16;
+typedef unsigned char BOOLEAN;
 typedef struct{
     UINT32 Data1;
     UINT16 Data2;
     UINT16 Data3;
     UINT8 Data4[8];
 }EFI_GUID;
-typedef void VOID;
-typedef __int32 INT32;
-typedef unsigned char UINT8;
-typedef unsigned short UINT16;
-typedef unsigned __int32 UINT32;
-typedef unsigned __int64 UINT64;
-typedef UINT64 UINTN;
-typedef unsigned short CHAR16;
-typedef unsigned char BOOLEAN;
+
 typedef VOID *EFI_HANDLE;
 typedef VOID *EFI_EVENT;
 typedef UINTN EFI_STATUS;
+
 typedef struct{
     UINT16 Signature;
     UINT32 Revision;
@@ -29,19 +31,18 @@ typedef struct{
     UINT32 CRC32;
     UINT32 Reserved;
 }EFI_TABLE_HEADER;
+
 typedef struct{
     EFI_TABLE_HEADER Hdr;
     // 本来ここ以下に色々続くが、使わない為に割愛
 }EFI_RUNTIME_SERVICES;
+
 typedef struct{
     EFI_TABLE_HEADER Hdr;
     // 本来ここ以下に色々続くが、使わない為に割愛
 }EFI_BOOT_SERVICES;
-typedef struct EFI_SIMPLE_TEXT_INPUT_PROTOCOL{
-    EFI_INPUT_RESET Reset;
-    EFI_INPUT_READ_KEY ReadKeyStroke;
-    EFI_EVENT WaitForKey;
-}EFI_SIMPLE_TEXT_INPUT_PROTOCOL;
+
+typedef struct EFI_SIMPLE_TEXT_INPUT_PROTOCOL EFI_SIMPLE_TEXT_INPUT_PROTOCOL;
 typedef EFI_STATUS (EFIAPI *EFI_INPUT_RESET)(
     IN EFI_SIMPLE_TEXT_INPUT_PROTOCOL *This,
     IN BOOLEAN ExtendedVerification
@@ -54,26 +55,13 @@ typedef EFI_STATUS (EFIAPI *EFI_INPUT_READ_KEY)(
     IN EFI_SIMPLE_TEXT_INPUT_PROTOCOL *This,
     OUT EFI_INPUT_KEY *Key
 );
-typedef struct{
-    INT32 MaxMode;
-    INT32 Mode;
-    INT32 Attribute;
-    INT32 CursorColumm;
-    INT32 CursorRow;
-    BOOLEAN CursorVisible;
-}EFI_SIMPLE_TEXT_OUTPUT_MODE;
-typedef struct EFI_SIMPLE_TEXT_OUTPUT_PROTOCOL{
-    EFI_TEXT_RESET Reset;
-    EFI_TEXT_STRING OutputString;
-    EFI_TEXT_TEST_STRING TestString;
-    EFI_TEXT_QUERY_MODE QueryMode;
-    EFI_TEXT_SET_MODE SetMode;
-    EFI_TEXT_SET_ATTRIBUTE SetAttribute;
-    EFI_TEXT_CLEAR_SCREEN ClearScreen;
-    EFI_TEXT_SET_CURSOR_POSITION SetCursorPosition;
-    EFI_TEXT_ENABLE_CURSOR EnableCursor;
-    EFI_SIMPLE_TEXT_OUTPUT_MODE *Mode;
+struct EFI_SIMPLE_TEXT_INPUT_PROTOCOL{
+    EFI_INPUT_RESET Reset;
+    EFI_INPUT_READ_KEY ReadKeyStroke;
+    EFI_EVENT WaitForKey;
 };
+
+typedef struct EFI_SIMPLE_TEXT_OUTPUT_PROTOCOL EFI_SIMPLE_TEXT_OUTPUT_PROTOCOL;
 typedef EFI_STATUS(EFIAPI *EFI_TEXT_ENABLE_CURSOR)(
     IN EFI_SIMPLE_TEXT_OUTPUT_PROTOCOL *This,
     IN BOOLEAN Visible
@@ -114,10 +102,32 @@ typedef EFI_STATUS(EFIAPI *EFI_TEXT_RESET)(
     IN EFI_SIMPLE_TEXT_OUTPUT_PROTOCOL *This,
     IN BOOLEAN ExtendedVerification
 );
+typedef struct{
+    INT32 MaxMode;
+    INT32 Mode;
+    INT32 Attribute;
+    INT32 CursorColumm;
+    INT32 CursorRow;
+    BOOLEAN CursorVisible;
+}EFI_SIMPLE_TEXT_OUTPUT_MODE;
+struct EFI_SIMPLE_TEXT_OUTPUT_PROTOCOL{
+    EFI_TEXT_RESET Reset;
+    EFI_TEXT_STRING OutputString;
+    EFI_TEXT_TEST_STRING TestString;
+    EFI_TEXT_QUERY_MODE QueryMode;
+    EFI_TEXT_SET_MODE SetMode;
+    EFI_TEXT_SET_ATTRIBUTE SetAttribute;
+    EFI_TEXT_CLEAR_SCREEN ClearScreen;
+    EFI_TEXT_SET_CURSOR_POSITION SetCursorPosition;
+    EFI_TEXT_ENABLE_CURSOR EnableCursor;
+    EFI_SIMPLE_TEXT_OUTPUT_MODE *Mode;
+};
+
 typedef struct {
     EFI_GUID VendorGuid;
     VOID    *VendorTable;
 } EFI_CONFIGURATION_TABLE;
+
 typedef struct{
     EFI_TABLE_HEADER Hdr;
     CHAR16 *FirmwareVendor;
@@ -133,6 +143,7 @@ typedef struct{
     UINTN NumberOfTableEntries;
     EFI_CONFIGURATION_TABLE *ConfigurationTable;
 }EFI_SYSTEM_TABLE;
+
 typedef EFI_STATUS
 (EFIAPI *EFI_IMAGE_ENTRY_POINT)(
     IN EFI_HANDLE ImageHandle,
--- UEFITest/UEFIMyselfTestCode/UEFITestCodeMyself.c
+++ UEFITest/UEFIMyselfTestCode/UEFITestCodeMyself.c
@@ -1,1 +1,7 @@
 #include "UEFITest.h"
+
+EFI_STATUS EfiApplicationEntryPoint(IN EFI_HANDLE ImageHandle, IN EFI_SYSTEM_TABLE *SystemTable){
+    (void)ImageHandle;
+    EFI_STATUS textout_status = SystemTable->ConOut->OutputString(SystemTable->ConOut,L"Hello,World!\n\r");
+    return 0;
+}
